ce24d471cbd5516c4b9f2045dac3f0a0a3283fbb
alielroby <ali.elroby2019@gmail.com>
2021-02-09 17:57:20 +0200

    Added Section 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Section1 from "./components/Section1";
 function App() {
   return (
     <div className="App">
       {/*============start Header==========*/}
       <Header />
+      {/*==================================*/}
+
+      {/*============start Section1==========*/}
+      <Section1 />
       {/*==================================*/}
     </div>
   );
---
+++ src/components/Section1.js
@@ -0,0 +1,31 @@
+import React from "react";
+function Section1() {
+  return (
+    //=======Start Section 1=======//
+    <div className="section1">
+      <div className="container">
+        <h2></h2>
+        <div className="row">
+          <div className="col-6">
+            <img src="img/img2.png" alt="" />
+          </div>
+          <div className="col-6"></div>
+        </div>
+        <div className="row">
+          <div className="col-6"></div>
+          <div className="col-6">
+            <img src="img/img3.png" alt="" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <img src="img/img4.png" alt="" />
+          </div>
+          <div className="col-6"></div>
+        </div>
+      </div>
+    </div>
+    //=============================//
+  );
+}
+export default Section1;
