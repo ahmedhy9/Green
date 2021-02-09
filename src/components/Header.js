94a37932eccf02237284614496659d65fd78641c
alielroby <ali.elroby2019@gmail.com>
2021-02-09 15:05:24 +0200

    Start Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+function Header(){
+    return(
+        //==========Start Header==========//
+        <div className="main-header">
+            {/* Start NavBar */}
+            <Navbar/>
+            {/* End NavBar */}
+        </div>
+        //===============================//
+    )
+}
+export default Header
